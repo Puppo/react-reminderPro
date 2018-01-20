f3cd550f95e6838f9a1c1340eb168e29559713e2
Luca Del Puppo <luca@delpuppo.net>
2018-01-20 10:28:28 +0100

    complete app with delete reminder and clear reminders

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,28 @@
-import {ADD_REMINDER} from '../constants';
+import {ADD_REMINDER, DELETE_REMINDER, CLEAR_REMINDERS} from '../constants';
 
-export const addReminder = (text) => {
+export function addReminder(text, dueDate) {
   const action = {
     type: ADD_REMINDER,
-    text
+    text,
+    dueDate
   };
+  console.log('adding in actions', action);
   return action;
 };
+
+export function deleteReminder(id) {
+  const action = {
+    type: DELETE_REMINDER,
+    id
+  };
+  console.log('deleting in actions', action);
+  return action;
+};
+
+export function clearReminders() {
+  const action = {
+    type: CLEAR_REMINDERS
+  };
+  console.log('clear reminders in actions', action);
+  return action;
+}
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { addReminder } from '../actions';
+import { addReminder, deleteReminder, clearReminders } from '../actions';
+import moment from 'moment';
 import '../index.css'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: ''
+      text: '',
+      dueDate: ''
     };
   }
 
   addReminder() {
-    this.props.addReminder(this.state.text);
+    this.props.addReminder(this.state.text, this.state.dueDate);
+  }
+
+  deleteReminder(id) {
+    this.props.deleteReminder(id)
   }
 
   renderReminders() {
@@ -23,7 +29,15 @@ class App extends Component {
           reminders.map(reminder => {
             return (
               <li key={reminder.id} className="list-group-item">
-                <div>{reminder.text}</div>
+                <div className="list-item">
+                  <div>{reminder.text}</div>
+                  <div><em>{moment(new Date(reminder.dueDate)).fromNow()}</em></div>
+                </div>
+                <div
+                  className="list-item delete-button"
+                  onClick={() => this.deleteReminder(reminder.id)}>
+                  &#x2715;
+                </div>
               </li>
             )
           })
@@ -45,6 +59,11 @@ class App extends Component {
               placeholder="I have to.."
               onChange={event => this.setState({text: event.target.value})}
               />
+            <input
+              className="form-control"
+              type="datetime-local"
+              onChange={event => this.setState({dueDate: event.target.value})}
+              />
           </div>
           <button
             type="button"
@@ -54,6 +73,12 @@ class App extends Component {
           </button>
         </div>
         { this.renderReminders() }
+        <div
+          className="btn btn-danger"
+          onClick={() => this.props.clearReminders() }
+          >
+          Clear Reminders
+        </div>
       </div>
     );
   }
@@ -66,4 +91,4 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps, {addReminder})(App);
+export default connect(mapStateToProps, { addReminder, deleteReminder, clearReminders })(App);
